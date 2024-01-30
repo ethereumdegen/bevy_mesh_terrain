86612cff4598d94043664f03bb07f803dda4e957
Admazzola <admazzola@gmail.com>
2024-01-30 13:43:14 -0500

    custom update rate

--- src/lib.rs
+++ src/lib.rs
@@ -20,14 +20,14 @@ pub mod terrain_material;
 
 
 pub struct TerrainMeshPlugin {
-    
+    task_update_rate: Duration
     
 }
 
 impl Default for TerrainMeshPlugin {
     fn default() -> Self {
         Self {
-           
+           task_update_rate: Duration::from_millis(250)
         }
     }
 }
@@ -39,15 +39,15 @@ impl Plugin for TerrainMeshPlugin {
         
         app.add_event::<ChunkEvent>();
         
-        app.add_systems(Update, activate_terrain_chunks .run_if(on_timer(Duration::from_millis(100)) )   );
-        app.add_systems(Update, destroy_terrain_chunks .run_if(on_timer(Duration::from_millis(100)) )   );
+        app.add_systems(Update, activate_terrain_chunks .run_if(on_timer(self.task_update_rate) )   );
+        app.add_systems(Update, destroy_terrain_chunks .run_if(on_timer(self.task_update_rate) )   );
         app.add_systems( Last , despawn_terrain_chunks ); 
         
-        app.add_systems(Update, finish_chunk_build_tasks.run_if(on_timer(Duration::from_millis(100)) )   );
+        app.add_systems(Update, finish_chunk_build_tasks.run_if(on_timer(self.task_update_rate) )   );
         
         app.add_systems(Update, build_active_terrain_chunks/*.after( update_terrain_chunks )*/);
         
-        app.add_systems( Update, spawn_chunk_collision_data .run_if(on_timer(Duration::from_millis(100)) )   );
+        app.add_systems( Update, spawn_chunk_collision_data .run_if(on_timer(self.task_update_rate) )   );
         
         app.add_systems(Update, load_height_map_data_from_image  ) ;
         app.add_systems(Update, load_terrain_texture_from_image  ) ;
