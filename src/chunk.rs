9b4ec13bb0655c6bd2faee6d59d3bbd2bfa4d2b2
Emi <Emanuel.boehm@gmx.de>
2024-04-04 19:22:06 +0200

    refactor chunk loading to use pathbufs instead

--- src/chunk.rs
+++ src/chunk.rs
@@ -262,18 +262,18 @@ pub fn initialize_chunk_data(
         let (terrain_config, terrain_data) = terrain_query.get(terrain_entity_id).unwrap();
 
         let chunk_id = chunk.chunk_id;
-
+        let file_name = format!("{}.png", chunk_id);
         //default_terrain/diffuse
-        let height_texture_folder_path = &terrain_config.height_folder_path;
-        let height_texture_path = format!("{}/{}.png", height_texture_folder_path, chunk_id);
-        println!("loading from {}", height_texture_path);
+        let height_texture_path = terrain_config.height_folder_path.with_file_name(file_name);
+        println!("loading from {}", height_texture_path.display());
 
         let height_map_image_handle: Handle<Image> = asset_server.load(height_texture_path);
 
         //default_terrain/splat
         let splat_texture_folder_path = &terrain_config.splat_folder_path;
-        let splat_texture_path = format!("{}/{}.png", splat_texture_folder_path, chunk_id);
-        println!("loading from {}", splat_texture_path);
+
+        let splat_texture_path = terrain_config.splat_folder_path.with_file_name(file_name);
+        println!("loading from {}", splat_texture_path.display());
 
         let splat_image_handle: Handle<Image> = asset_server.load(splat_texture_path);
 
