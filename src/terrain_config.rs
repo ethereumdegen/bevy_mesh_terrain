e372e8bde05c44777889a76de374e0a41ffe94d0
Emi <Emanuel.boehm@gmx.de>
2024-04-04 19:21:16 +0200

    replace String with Pathbufs

--- src/terrain_config.rs
+++ src/terrain_config.rs
@@ -12,6 +12,7 @@ use serde::{Deserialize, Serialize};
 
 use std::fs::File;
 use std::io::Read;
+use std::path::PathBuf;
 
 #[derive(Component, Deserialize, Serialize, Clone)]
 pub struct TerrainConfig {
@@ -29,10 +30,10 @@ pub struct TerrainConfig {
     pub use_greedy_mesh: bool,
 
     pub texture_image_sections: u32,
-    pub diffuse_folder_path: String,
-    pub height_folder_path: String,
-    pub splat_folder_path: String,
-    pub collider_data_folder_path: String,
+    pub diffuse_folder_path: PathBuf,
+    pub height_folder_path: PathBuf,
+    pub splat_folder_path: PathBuf,
+    pub collider_data_folder_path: PathBuf,
 }
 
 impl Default for TerrainConfig {
